c7ea8e6c56bf5ebbe976ec0f64e1fd7aa033f56b
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-08-13 13:19:53 +0300

    start

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = { 
+    output: 'export',  // <=== enables static exports
+    images: { unoptimized: true },
+    distDir: 'dist',
+    reactStrictMode: true,
+    basePath: isProd ? '/car-rental-nextjs' : '',
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
+};
 
 export default nextConfig;
